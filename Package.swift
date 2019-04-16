1f9dfa3de5c003c95a6bc805a1f9a968ef16a0ae
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-04-16 20:27:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "PerfectSessionMySQL",
-    products: [],
+    products: [
+        .library(name: "PerfectSessionMySQL", targets: ["PerfectSessionMySQL"])
+    ],
     dependencies: [
         .package(url: "https://github.com/123FLO321/Perfect-Session.git", .exact("3.1.6")),
         .package(url: "https://github.com/123FLO321/Perfect-MySQL.git", .exact("3.2.2"))
+    ],
+    targets: [
+        .target(name: "PerfectSessionMySQL", dependencies: ["PerfectSession", "PerfectMySQL"])
+   
     ]
 )
