224661ade444436d0be1361557aaddac5b47960a
SkOODaT <8633932+SkOODaT@users.noreply.github.com>
2022-03-21 12:35:43 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "PerfectSessionMySQL", targets: ["PerfectSessionMySQL"])
     ],
     dependencies: [
-        .package(name: "PerfectSession", url: "https://github.com/123FLO321/Perfect-Session.git", .branch("swift5")),
-        .package(name: "PerfectMySQL", url: "https://github.com/123FLO321/Perfect-MySQL.git", .branch("swift5")),
+        .package(name: "PerfectSession", url: "https://github.com/SkOODaT/Perfect-Session.git", .branch("SKOODAT")),
+        .package(name: "PerfectMySQL", url: "https://github.com/SkOODaT/Perfect-MySQL.git", .branch("SKOODAT")),
     ],
     targets: [
         .target(name: "PerfectSessionMySQL", dependencies: ["PerfectSession", "PerfectMySQL"])
